67d97fc79a4a6e8cd0de0563afa9b1fba9e9001b
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-09-30 12:22:26 +0200

    changing brackets

--- src/features/personalHomepage/PersonalHomepage/Portfolio/Content/Repositories/index.js
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/Content/Repositories/index.js
@@ -10,32 +10,34 @@ import {
 } from "./styled";
 
 export const Repositories = ({ repositories }) => {
-  <List>
-    {repositories.map(({ id, name, description, homepage, html_url }) => (
-      <Tile key={id}>
-        <Name>{name}</Name>
-        <Description>{description}</Description>
-        <Links>
-          {!!homepage && (
+  return (
+    <List>
+      {repositories.map(({ id, name, description, homepage, html_url }) => (
+        <Tile key={id}>
+          <Name>{name}</Name>
+          <Description>{description}</Description>
+          <Links>
+            {!!homepage && (
+              <LinksRow>
+                <dt>Demo:</dt>
+                <LinksValue>
+                  <Link target="_blank" rel="noreferrer" href={homepage}>
+                    {homepage}
+                  </Link>
+                </LinksValue>
+              </LinksRow>
+            )}
             <LinksRow>
-              <dt>Demo:</dt>
+              <dt>Code:</dt>
               <LinksValue>
-                <Link target="_blank" rel="noreferrer" href={homepage}>
-                  {homepage}
+                <Link target="_blank" rel="noreferrer" href={html_url}>
+                  {html_url}
                 </Link>
               </LinksValue>
             </LinksRow>
-          )}
-          <LinksRow>
-            <dt>Code:</dt>
-            <LinksValue>
-              <Link target="_blank" rel="noreferrer" href={html_url}>
-                {html_url}
-              </Link>
-            </LinksValue>
-          </LinksRow>
-        </Links>
-      </Tile>
-    ))}
-  </List>;
+          </Links>
+        </Tile>
+      ))}
+    </List>
+  );
 };
