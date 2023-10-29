44d9d8af7a0526ba811283f631678b29cacbdc7b
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-10-29 20:56:35 +0100

    changing margin top in Container

--- src/features/personalHomepage/PersonalHomepage/styled.js
+++ src/features/personalHomepage/PersonalHomepage/styled.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   max-width: 1248px;
   padding: 16px;
   margin: auto;
-  margin-top: 80px;
+  margin-top: 40px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     padding: 8px;
